70b440b683919a4d6baf378b869922b84c73fe9b
Isaiah Fernandez <isaiah.fernandez@stu.ocsb.ca>
2022-09-16 09:04:40 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,12 @@
+// Copyright (c) 2022 Isaiah fernandez All rights reserved
+// .
+// Created by: Isaiah Fernandez
+// Date: September 15, 2022
+// This program writes "Hello, World!" in style
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Ann!" << std::endl;
+    std::cout << "This class is so fun!" << std::endl;
+}
